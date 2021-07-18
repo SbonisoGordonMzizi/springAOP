d47e5a01698d185b048a8df2db6f83b06fc4bf1f
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2021-07-18 15:48:20 +0200

    implementing within,args PointCut and JoinPoint

--- src/main/java/com/gordonmzizi/aspects/LoggingAspect.java
+++ src/main/java/com/gordonmzizi/aspects/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.gordonmzizi.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -10,14 +11,22 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspect {
     @Before("dummy()")
-    public void interceptBefore(){
-        System.out.println("Logging before executing business logic");
+    public void interceptBefore(JoinPoint joinPoint){
+        System.out.println("Logging before executing business logic "+joinPoint.getSignature());
     }
 
     @After("dummy()")
-    public void interceptAfter(){
-        System.out.println("Logging after executing business logic");
+    public void interceptAfter(JoinPoint joinPoint){
+        System.out.println("Logging after executing business logic "+joinPoint.getSignature());
     }
-    @Pointcut("execution(public void com.gordonmzizi.businesslogic.*.*(..))")
+    @Before("args(name)")
+    public void setAccountNumOne(String name){
+        System.out.println("Account modifying call "+name);
+    }
+    @After("args(name)")
+    public void setAccountNumTwo(String name){
+        System.out.println("Account modified by "+name );
+    }
+    @Pointcut("execution(public void com.gordonmzizi.*.*.*(..))")
     public void dummy(){}
 }
